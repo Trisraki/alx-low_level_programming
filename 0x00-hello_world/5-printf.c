a7b4b7781b653af4934bd79a9fcf002e2994bb83
Raquel Kenya <trisraquel1@gmail.com>
2022-12-08 19:19:45 -0800

    5printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- *  Return: Always 0 (Success)
- */
+ * main - Entry point* main - Entry point
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 			return (0);
 }
