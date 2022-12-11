051361d232fd6e24922b50aa71b3ce7f27242ec8
Raquel Kenya <trisraquel1@gmail.com>
2022-12-11 11:26:02 -0800

    hot fix 4 put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Entry point
- *  
+ * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	return (0);
 }
