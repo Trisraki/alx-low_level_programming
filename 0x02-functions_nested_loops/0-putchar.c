70b792ee1e011c66654028c92eb2c3b7dcd20e4e
Raquel Kenya <trisraquel1@gmail.com>
2022-12-15 15:18:05 -0800

    hot putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@ int main(void)
 	_putchar('_');
         _putchar('p');
 	_putchar('u');
-	_putchar('t);
+	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
 	     
-	return (0)
+	return (0);
 
 }
