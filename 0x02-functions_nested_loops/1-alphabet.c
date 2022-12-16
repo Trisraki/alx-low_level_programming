ca98fe5db32021d31c640e9beb537b0963e6f040
Raquel Kenya <trisraquel1@gmail.com>
2022-12-15 17:40:03 -0800

    void print_alphabet(void);

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_alphabet -> prints the lowecase alphabets
+*/
+
+void prints_alphabet(void)
+{
+	int j;
+
+	for (j= 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
