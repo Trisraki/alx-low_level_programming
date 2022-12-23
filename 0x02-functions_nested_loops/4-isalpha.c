818d9f72e26f64938a2d86aea205f70d617e91fa
Raquel Kenya <trisraquel1@gmail.com>
2022-12-23 13:33:58 -0800

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,10 +6,11 @@
   *
   * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
   **/
+
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-			return (1);
+		return (1);
 	else
-			return (0);
+		return (0);
 }
