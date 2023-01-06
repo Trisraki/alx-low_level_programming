00c858dab85937192f94873834ddfcafbf683b76
Raquel Kenya <trisraquel1@gmail.com>
2023-01-05 23:09:14 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,7 +3,7 @@
 /**
  * print_times_table - Prints the times table of the input,
  * starting with 0.
- * @n: The value of the times table to be printed.
+ *  @n: The value of the times table to be printed.
  **/
 void print_times_table(int n)
 {
