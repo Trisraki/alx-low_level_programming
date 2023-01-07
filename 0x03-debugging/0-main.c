3db962314c642859436084c4bc8106ca5ed4db82
Raquel Kenya <trisraquel1@gmail.com>
2023-01-07 15:03:23 -0800

    0-main.c, main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,16 @@
-#ifndef main_h
-#define main_h
+#include "main.h"
 
-#include <stdio.h>
+/**
+ * main - Test function for positive or negative
+ * Return: 0
+ **/
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
+int main(void)
+{
+	int i;
 
-#endif
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
