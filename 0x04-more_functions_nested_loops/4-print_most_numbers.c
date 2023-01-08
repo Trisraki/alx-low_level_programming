5fcc4dd0fefb927484a9a22e4f6e310d21943954
Raquel Kenya <trisraquel1@gmail.com>
2023-01-07 18:44:25 -0800

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints
- * owned by Bwave ICT
- * Return: void
+ * print_most_numbers - checks for checks for a digit (0 through number 9)
  *
+ * Return: Always 0.
  **/
-
 void print_most_numbers(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= '9 ; c++)
+	for (c = 48; c < 58; c++)
 	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
+		if (c != 50)
+		{
+			if (c != 52)
+			{
+				_putchar(c);
+			}
+		}
 	}
 	_putchar('\n');
 }
