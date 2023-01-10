73b06dc82271ffd408d8b8fc6e52e29de1669211
Raquel Kenya <trisraquel1@gmail.com>
2023-01-10 15:44:08 -0800

    6-puts2c hot fx

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,26 +6,19 @@
  * @str: input
  * Return: print
  **/
-
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+	int count = 0;
 
-	while (*y != '\0')
+	while (count >= 0)
 	{
-		y++;
-		longi++;
-	}
-	t = longi - 1;
-	for (o = 0 ; o <= t ; o++)
-	{
-		if (o % 2 == 0)
+		if (str[count] == '\0')
 		{
-			_putchar(str[o]);
-		}
 			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
 	}
 }
