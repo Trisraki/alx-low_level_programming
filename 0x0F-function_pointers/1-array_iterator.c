6b4b1f76eb616c35bc985d88685e62e3e30b0a43
Raquel Kenya <trisraquel1@gmail.com>
2023-01-22 15:03:47 -0800

    1-array_iterator.c fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,12 +11,13 @@
  **/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	 unsigned int i;
+	unsigned int i;
 
-	 if (array == NULL || action == NULL)
-		 return;
-	 for (i = 0; i < size; i++)
-	 {
-		 action(array[i]);
-	 }
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
