bb08fcaf732fd522f7ee6bc26d6fdd7c44843ad1
Raquel Kenya <trisraquel1@gmail.com>
2023-02-03 13:35:42 -0800

    101-print_listint_safe.c fix

---
+++ 0x13-more_singly_linked_lists/103-main.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+	listint_t *head2;
+	listint_t *node;
+
+	head2 = NULL;
+	add_nodeint(&head2, 0);
+	add_nodeint(&head2, 1);
+	add_nodeint(&head2, 2);
+	add_nodeint(&head2, 3);
+	add_nodeint(&head2, 4);
+	add_nodeint(&head2, 98);
+	add_nodeint(&head2, 402);
+	add_nodeint(&head2, 1024);
+	print_listint_safe(head2);
+	node = find_listint_loop(head2);
+	if (node != NULL)
+	{
+		printf("Loop starts at [%p] %d\n", (void *)node, node->n);
+	}
+	print_listint_safe(&head);
+	return (0);
+}
