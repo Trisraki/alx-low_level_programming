a8a88d0d118c11057b0fae1f034ba42dff8fc646
Raquel Kenya <trisraquel1@gmail.com>
2023-02-14 08:24:21 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 /**
- * main -> assign a random number to the variable n each time it is executed
- * and print the last digit of the number stored in the variable n
- * Return: Always 0 (Success)
- **/
+ * main - print if the number is postive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
 int main(void)
 {
-int n;
+	char n;
 
-for (n = 0; n < 10; n++)
-{
-printf("%d", n);
-}
-printf("\n");
-return (0);
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+
+	putchar('\n');
+
+	return (0);
 }
