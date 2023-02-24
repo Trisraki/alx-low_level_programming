3dfd709d921af987654f1dfb18342d2ac739dc0f
Trisraki <trisraquel1@gmail.com>
2023-02-23 16:35:05 -0800

    update

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,1 @@
-0x09 Static Libraries
+0x09-static_libraries
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
 
-int _putchar(char c);	
+int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
@@ -21,6 +22,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-
 #endif
