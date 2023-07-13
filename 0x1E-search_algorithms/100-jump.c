a7495bd6c75fed3db75113d5a655c71cd8665385
Trisraki <trisraquel1@gmail.com>
2023-07-13 16:27:47 -0700

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,44 +1,73 @@
 #include "search_algos.h"
 
 /**
- * jump_search - a function that searches for a value in a
- * sorted array of integers using the Jump search algorithm.
- *
- * @array: a pointer to the first element of the array to search in
- * @size: the number of elements in array
- * @value: the value to search for
- *
- * Return: index of value else -1
+ * print_range - print at each value check
+ * @i: index before jump
+ * @j: index after jump
  */
+void print_range(size_t i, size_t j)
+{
+	printf("Value found between indexes [%lu] and [%lu]\n", i, j);
+}
 
+/**
+ * print_i - print idx each check
+ * @array: array
+ * @i: index checked
+ */
+void print_i(int *array, size_t i)
+{
+	printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+}
+
+/**
+ * jump_search - perform a jump search ie find square root,
+ * jump steps to find match, end linear search
+ * @array: given array of ints
+ * @size: size of array
+ * @value: value to search for
+ * Return: index at which value's found
+ */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t blocksize, end, i, start;
+	size_t jumps = sqrt(size);
+	size_t i = 0;
+	size_t j = 0;
 
-	if (array == NULL)
-		return (-1);
-
-	blocksize = (size_t)sqrt((double)size);
-	start = 0;
-	end = blocksize;
-
-	while ((array[end] <= value) && (end < size))
+	if (!array)
+		goto KILL;
+	while (j <= size)
 	{
-		printf("Value cheacked array [%lu] = [%lu]\n", end, end);
-		start = end;
-		end = end + blocksize;
+		if (j != 0)
+			print_i(array, i);
+		if (array[j] >= value)
+		{
+			print_range(i, j);
+			while (i <= j)
+			{
+				print_i(array, i);
+				if (array[i] == value)
+					return (i);
+				i++;
+			}
+			goto KILL;
+		}
+		if (j + jumps + jumps > size)
+		{
+			print_i(array, j);
+			print_range(j, j + jumps);
+			while (j < size)
+			{
+				print_i(array, j);
+				if (array[j] == value)
+					return (j);
+				j++;
+			}
+			goto KILL;
 
-		if (end > size - 1)
-			end = size;
+		}
+		i = j;
+		j += jumps;
 	}
-
-	printf("Value found between indexes [%lu] and [%lu]\n", start, end);
-
-	for (i = start; i <= end - 1; i++)
-	{
-		if (array[i] == value)
-			return (i);
-	}
-
-	return (-1);
+KILL:	return (-1);
 }
